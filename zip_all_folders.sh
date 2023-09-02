2e38339264373331b341f387f75f61dcb78966db
Cyrus Chan <kb041204@hotmail.com>
2023-09-03 03:06:22 +0800

    support directories with sapce

--- zip_all_folders.sh
+++ zip_all_folders.sh
@@ -6,17 +6,15 @@
 #  The folder itself will not be zip
 #
 #  Made by Cyrus Chan on 2023/08/29
+#  Modified on 2023/09/03 to support directories with space
 ###################################################################################
 
-directories=$(find . -type d)
-delete="."
-
-directories=( "${directories[@]/$delete}" )
-for directory in $directories
+readarray -d '' directories < <(find . -type d -print0)
+for directory in "${directories[@]:1}"
 do
 	file_name=$(echo $directory | cut -c 3-)
 	file_name="${file_name}.zip"
 	directory="${directory}/*"
-	7z a $file_name $directory
+	7z a "${file_name}" "${directory}"
 	echo "${file_name} OK"
 done
